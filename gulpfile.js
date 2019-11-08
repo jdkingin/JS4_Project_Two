d777c960c6455f9f3bf6907acbf3483e04def804
jdkingin <jaideeking5@gmail.com>
2019-11-08 15:42:52 -0500

    Gulp file dev code complete

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const {src, dest, watch, series, parallel} = require('gulp');
+const md = require ('gulp-markdown');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const clean = require('delete');
+const typeScript = require('gulp-typescript');
+const minify = require('gulp-terser');
+const browseSync = require('browser-sync');
+const wrap = require('gulp-wrap');
+const frontMatter = require('gulp-front-matter');
+const fs = require('fs');
+const $ = require('jquery');
+
+function html_task()
+{
+    return src('source/*.html')
+                .pipe(dest('prod/'));
+}
+
+function js_task()
+{
+    return src(['source/scripts/*.js', 'source/scripts/*.ts'])
+            .pipe(typeScript({target: 'ES5', allowJs: true}))
+            .pipe(concat('main.js'))
+            .pipe(minify())
+            .pipe(dest('prod/scripts/'));
+}
+
+function css_task()
+{
+    return src('source/styles/*.scss')
+            .pipe(sass())
+            .pipe(dest('prod/styles/'));
+}
+
+function markdown_task()
+{
+    return src('source/pages/*.md')
+            .pipe(md())
+            .pipe(dest('source/'));
+}
+
+function image_task()
+{
+    return src('source/images/*png')
+            .pipe(dest('prod/images/'))
+}
+
+function watch_task()
+{
+    watch('source/*.html', series(html_task, reload_task));
+    watch(['source/scripts/*.js', 'source/scripts/*.ts'], series(js_task, reload_task));
+    watch('source/images/*.png', series(image_task, reload_task));
+    watch('source/styles/*.scss', series(css_task, reload_task));
+}
+
+function sync_task(cb)
+{
+    browseSync.init({server: {baseDir: 'prod/'}});
+    (cb);
+}
+
+function reload_task(cb){
+    browseSync.reload()
+    cb();
+}
+
+function remove_task(cb)
+{
+    clean('prod/**', cb)
+}
+
+exports.remove = remove_task;
+exports.watch = watch_task;
+exports.build = series(remove_task, markdown_task, parallel(html_task, js_task, css_task, image_task));
+exports.default = parallel(exports.build, watch_task, sync_task);
